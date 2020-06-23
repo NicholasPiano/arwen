3bdff334e78180d3dde7429767f140ae26f7ce1a
Nicholas Piano <nicholas.d.piano@gmail.com>
2020-06-23 13:06:12 +0200

    Update package versions for development

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
 import { API, RestAPI } from './src/api';
+import { Model, Manager } from './src/model';
 import { reducer } from './src/utilities';
 
 export {
   API,
   RestAPI,
+  Model,
+  Manager,
   reducer,
 };
